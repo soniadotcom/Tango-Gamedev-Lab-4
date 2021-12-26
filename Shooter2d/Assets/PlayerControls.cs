ac969defc3f88f1770a1abeb5b176bf7d9347710
LevSv <mr.svitelskiy@gmail.com>
2021-12-26 12:23:37 +0200

    first commit

---
+++ Shooter2d/Assets/PlayerControls.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControls : MonoBehaviour
+{
+
+    public KeyCode moveUp;
+    public KeyCode moveDown;
+
+    float speed = 10;
+
+    void Update()
+    {
+        
+    }
+}
